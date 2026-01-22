212de7a586a348bf57ae99435eab820e4a9a40ad
reddynie11 <reddy.nie11@gmail.com>
2026-01-22 07:52:30 +0000

    data_quality_rules

--- LakeFlow_Project/transformations/bronze/customer_ingestion.py
+++ LakeFlow_Project/transformations/bronze/customer_ingestion.py
@@ -1,5 +1,11 @@
 import dlt
 
+customer_rules = {
+    "rule_1" : "customer_id IS NOT NULL",
+    "rule_2" : "customer_name IS NOT NULL"
+}
+
 @dlt.table(name="customers_bronze")
+@dlt.expect_all(customer_rules)
 def customers_bronze():
     return spark.readStream.table("lakeflow_catalog.source.customers")
--- LakeFlow_Project/transformations/bronze/product_ingestion.py
+++ LakeFlow_Project/transformations/bronze/product_ingestion.py
@@ -1,8 +1,13 @@
 import dlt
 
+product_rules = {
+    "rule_1" : "product_id is NOT NULL",
+    "rule_2" : "price >= 0"
+}
 # Ingesting products into Bronze layer
 @dlt.table(
     name="products_bronze"
 )
+@dlt.expect_all(product_rules)
 def products_bronze():
     return spark.readStream.table("lakeflow_catalog.source.products")
