d20756faf7bb40008d02376f7610bcb5659512fc
reddynie11 <reddy.nie11@gmail.com>
2026-01-22 11:46:33 +0000

    customer_transform

--- LakeFlow_Project/transformations/silver/customer_transform.py
+++ LakeFlow_Project/transformations/silver/customer_transform.py
@@ -1,20 +1,14 @@
 import dlt
 from pyspark.sql.functions import col, upper
 
-# ---------------------------------------------------
-# Step 1: Create a Silver TABLE (base streaming table)
-# ---------------------------------------------------
 
 dlt.create_streaming_table(
     name="customers_silver"
 )
 
-# ---------------------------------------------------
-# Step 2: Create a Silver VIEW (transformation logic)
-# ---------------------------------------------------
 
 @dlt.view(
-    name="customers_silver_view"
+    name="customers_stg_view"
 )
 def customers_silver_view():
     df = spark.readStream.table(
@@ -26,13 +20,10 @@ def customers_silver_view():
     )
     return df
 
-# ---------------------------------------------------
-# Step 3: Apply CDC logic into Silver TABLE
-# ---------------------------------------------------
 
 dlt.create_auto_cdc_flow(
     target="customers_silver",
-    source="customers_silver_view",
+    source="customers_stg_view",
     keys=["customer_id"],
     sequence_by="last_updated",
     stored_as_scd_type=1,
